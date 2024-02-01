78147c800aa1d858a1817d97ed50132cb205d661
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-02-01 19:52:26 +0700

    Customer CRUD

--- resources/views/page/masterdata/customer.blade.php
+++ resources/views/page/masterdata/customer.blade.php
@@ -167,7 +167,9 @@
         <div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             @php
-                $subdata = Customer::where('id', $item->id)->first();
+                @if ($data != null)
+                    $subdata = Customer::where('id', $item->id)->first();
+                @endif
             @endphp
             <div class="modal-content">
             <div class="modal-header">
