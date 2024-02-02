514c085f4678be4d3fc40d46ec0649d0df3747f3
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-02-02 08:45:16 +0700

    Karyawan CRUD

--- resources/views/page/masterdata/karyawan.blade.php
+++ resources/views/page/masterdata/karyawan.blade.php
@@ -241,7 +241,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="{{Route('karyawan.store')}}" method="POST">
+                <form action="{{Route('karyawan.store', $item->id)}}" method="POST">
                 @csrf
                 <div class="mb-3">
                     <label class="form-label">Nama</label>
