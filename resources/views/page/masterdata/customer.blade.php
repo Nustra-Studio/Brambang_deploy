c140d3f0ab80fb5b892c57b10a0f62cb040fe5ad
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-02-02 13:18:51 +0700

    Karyawan CRUD

--- resources/views/page/masterdata/customer.blade.php
+++ resources/views/page/masterdata/customer.blade.php
@@ -105,126 +105,127 @@
             </div>
             <div id="checkboxTable">
             <!-- start LOOP -->
-            <div class="card mb-2">
-                @if (session('status'))
-                    <div class="alert alert-primary">
-                        {{ session('status')}}
-                    </div>
-                @elseif (session('hapus'))
-                    <div class="alert alert-danger">
-                        {{ session('hapus')}}
-                    </div>
-                @endif
-                @foreach ($data as $item)
-                <div class="card-body py-4 py-lg-0 sh-lg-8">
-                <div class="row g-0 h-100 align-content-center">
-                    <div class="col-11 col-lg-2 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-1 order-lg-1 h-lg-100 position-relative">
-                    <div class="text-muted text-small d-lg-none">Nama</div>
-                    <a href="#" class="text-truncate h-100 d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#discountDetailModal">
-                        {{ $item->name }}
-                    </a>
-                    </div>
-                    <div class="col-6 col-lg-2 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-3 order-lg-2">
-                    <div class="text-muted text-small d-lg-none">No. HP</div>
-                    <div class="text-alternate">{{$item->hp}}</div>
-                    </div>
-                    <div class="col-6 col-lg-4 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-4 order-lg-3">
-                    <div class="text-muted text-small d-lg-none">Alamat</div>
-                    <div class="text-alternate">{{$item->address}}</div>
-                    </div>
-                    <div class="col-6 col-lg-2 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-5 order-lg-4">
-                    <div class="text-muted text-small d-lg-none">Jumlah Transaksi</div>
-                    <div class="text-alternate">12</div>
-                    </div>
-                    <div class="col-6 col-lg-1 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-last order-lg-5">
-                    
-                    </div>
-                    <div class="col-1 col-lg-1 d-flex flex-column justify-content-center align-items-lg-end mb-2 mb-lg-0 order-2 text-end order-lg-last">
-                    <div class="container-fluid d-lg-flex flex-lg-row gap-1 gap-lg-2 justify-content-lg-end">
-                        <div class="col">
-                            <button class="btn btn-primary d-flex justi fy-content-center align-items-center border shadow fw-bold p-3 p-lg-2 p-xl-3" data-bs-toggle="modal" data-bs-target="#editUserModal">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </button>
+                <div class="card mb-2">
+                    @if (session('status'))
+                        <div class="alert alert-primary">
+                            {{ session('status')}}
                         </div>
-                        <div class="col">
-                            <button class="btn btn-danger d-flex justify-content-center align-items-center border shadow fw-bold p-3 p-lg-2 p-xl-3" data-bs-toggle="modal" data-bs-target="#deleteUserModal">
-                                <i class="fa-solid fa-trash"></i>
-                            </button>
+                    @elseif (session('hapus'))
+                        <div class="alert alert-danger">
+                            {{ session('hapus')}}
+                        </div>
+                    @endif
+                    @foreach ($data as $item)
+                    <div class="card-body py-4 py-lg-0 sh-lg-8">
+                        <div class="row g-0 h-100 align-content-center">
+                            <div class="col-11 col-lg-2 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-1 order-lg-1 h-lg-100 position-relative">
+                                <div class="text-muted text-small d-lg-none">Nama</div>
+                                    <a href="#" class="text-truncate h-100 d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#discountDetailModal">
+                                        {{ $item->name }}
+                                    </a>
+                                </div>
+                                    <div class="col-6 col-lg-2 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-3 order-lg-2">
+                                        <div class="text-muted text-small d-lg-none">No. HP</div>
+                                        <div class="text-alternate">{{$item->hp}}</div>
+                                    </div>
+                                <div class="col-6 col-lg-4 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-4 order-lg-3">
+                                <div class="text-muted text-small d-lg-none">Alamat</div>
+                                <div class="text-alternate">{{$item->address}}</div>
+                                </div>
+                                <div class="col-6 col-lg-2 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-5 order-lg-4">
+                                <div class="text-muted text-small d-lg-none">Jumlah Transaksi</div>
+                                <div class="text-alternate">12</div>
+                                </div>
+                                <div class="col-6 col-lg-1 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-last order-lg-5">
+                                
+                                </div>
+                                <div class="col-1 col-lg-1 d-flex flex-column justify-content-center align-items-lg-end mb-2 mb-lg-0 order-2 text-end order-lg-last">
+                                <div class="container-fluid d-lg-flex flex-lg-row gap-1 gap-lg-2 justify-content-lg-end">
+                                    <div class="col">
+                                        <button class="btn btn-primary d-flex justi fy-content-center align-items-center border shadow fw-bold p-3 p-lg-2 p-xl-3" data-bs-toggle="modal" data-bs-target="#editUserModal">
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </button>
+                                    </div>
+                                    <div class="col">
+                                        <button class="btn btn-danger d-flex justify-content-center align-items-center border shadow fw-bold p-3 p-lg-2 p-xl-3" data-bs-toggle="modal" data-bs-target="#deleteUserModal">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                        
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Edit Data Customer</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="{{Route('customer.update', $item->id)}}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                <div class="mb-3">
+                                    <label class="form-label">Nama</label>
+                                    <input type="text" class="form-control" name="name" value="{{$item->name}}" />
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">No hp</label>
+                                    <input type="text" class="form-control" name="hp" value="{{$item->hp}}" />
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Alamat</label>
+                                    <input type="text" class="form-control" name="address" value="{{$item->address}}" />
+                                </div>
+                                
+                            </div>
+                            <div class="modal-footer border-1">
+                                <button class="btn btn-icon btn-icon-end btn-primary" type="submit">
+                                    <span>Save</span>
+                                    <i data-acorn-icon="save"></i>
+                                </button>
+                            </div>
+                        </form>
+                            </div>
+                        </div>
                     </div>
+                    <div class="modal fade" id="deleteUserModal" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title fw-bold">Hapus Data?</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Konfirmasi Hapus Data
+                            </div>
+                            <div class="modal-footer">
+                                <form action="{{Route('customer.destroy', $item->id)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger border shadow" type="submit">Hapus</button>
+                                    <button class="btn btn-primary border-1" data-bs-dismiss="modal">Batal</button>
+                                </form>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
-                </div>
-                    
-            </div>
             </div>
         </div>
         </div>
         <!-- Discount List End -->
 
         <!-- Discount Detail Modal Start -->
-        <div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-           
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Edit Data Customer</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="{{Route('customer.update', $item->id)}}" method="POST">
-                    @csrf
-                    @method('PUT')
-                <div class="mb-3">
-                    <label class="form-label">Nama</label>
-                    <input type="text" class="form-control" name="name" value="{{$item->name}}" />
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">No hp</label>
-                    <input type="text" class="form-control" name="hp" value="{{$item->hp}}" />
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Alamat</label>
-                    <input type="text" class="form-control" name="address" value="{{$item->address}}" />
-                </div>
-                
-            </div>
-            <div class="modal-footer border-1">
-                <button class="btn btn-icon btn-icon-end btn-primary" type="submit">
-                    <span>Save</span>
-                    <i data-acorn-icon="save"></i>
-                </button>
-            </div>
-        </form>
-            </div>
-        </div>
-        </div>
+        
 
         <!-- Discount Detail Modal End -->
 
         <!-- Delete Modal -->
-        <div class="modal fade" id="deleteUserModal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title fw-bold">Hapus Data?</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Konfirmasi Hapus Data
-            </div>
-            <div class="modal-footer">
-                <form action="{{Route('customer.destroy', $item->id)}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger border shadow" type="submit">Hapus</button>
-                    <button class="btn btn-primary border-1" data-bs-dismiss="modal">Batal</button>
-                </form>
-            </div>
-            </div>
-                @endforeach
-        </div>
-        </div>
+        
         <!-- Delete Modal End -->
 
         <!-- Discount Add Modal Start -->
