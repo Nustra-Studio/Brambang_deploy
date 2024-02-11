9d9ddf9c4b020cc35b61fd7140b97c408952a165
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-02-11 23:24:48 +0700

    penjualan

--- resources/views/page/fitur/penjualan.blade.php
+++ resources/views/page/fitur/penjualan.blade.php
@@ -158,8 +158,8 @@
                                 <div class="text-alternate">{{$customer->name}}</div>
                             </div>
                             <div class="col-1 col-lg-1 d-flex flex-column justify-content-center mb-2 mb-lg-0 order-last order-lg-6">
-                                <div class="col">
-                                    <button class="btn btn-primary d-flex justi fy-content-center align-items-center border shadow fw-bold p-lg-2 p-xl-3" data-bs-toggle="modal" data-bs-target="#editUserModal{{$item->id}}">
+                                <div class="col pt-lg-2">
+                                    <button class="btn btn-primary d-flex justify-content-center align-items-center border shadow fw-bold p-lg-2 p-xl-3" data-bs-toggle="modal" data-bs-target="#editUserModal{{$item->id}}">
                                         <i class="fa-solid fa-pen-to-square"></i>
                                     </button>
                                 </div>
